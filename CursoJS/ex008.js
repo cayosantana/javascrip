11214b34dcddc2c754ba59aea546fddfb3a04e2a
Cayo Santana <cc.santana.js@gmail.com>
2023-10-09 18:08:22 -0300

    criei e Atualizei

---
+++ CursoJS/ex008.js
@@ -0,0 +1,6 @@
+var vel = 20
+if (vel > 60) {
+    console.log(`Você ultrapassou o limite de velociade da via que é de 60 km/h`)
+    console.log(`Você está MULTADO!`)
+}
+console.log(`A velociadade do seu carro é de: ${vel} Km/h`)
---
+++ CursoJS/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais} você é...`)
+if (pais == 'Brasil') {
+    console.log('Você é Brasileiro(a)!')
+} else {
+    console.log('Você é Estrangeiro(a)!')
+}
