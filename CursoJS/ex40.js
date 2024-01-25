e1098e7fa236d786ae109db98cb5f5153744aac8
Cayo Santana <102827350+cayosantana@users.noreply.github.com>
2024-01-25 16:41:15 -0300

    Create ex40.js

---
+++ CursoJS/ex40.js
@@ -0,0 +1,18 @@
+function soma(n1, n2) {
+    return n1 + n2
+}
+function sub(n1, n2) {
+    return n1 - n2
+}
+
+function main() {
+    const n1 = 30
+    const n2 = 25
+    const somaF = soma(n1, n2)
+    const subF = sub(n1, n2)
+    
+    console.log(somaF)
+    console.log(subF)
+}
+main()
+
